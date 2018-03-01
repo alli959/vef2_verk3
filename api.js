6d5e11a7f0e84ce3a4f06020b874e3253b3e5255
alli959 <alg35@hi.is>
2018-03-01 03:20:54 +0000

    alveg að koma

--- api.js
+++ api.js
@@ -10,10 +10,92 @@ const {
 
 const router = express.Router();
 
+
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-/* todo útfæra api */
+const data = [
+   {  id: 1, title: 'Foo' },
+   {  id: 2, title: 'Bar'}
+];
+
+router.get('/', (req, res) => {
+  res.json(readAll());
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const item = readOne(id);
+
+  if(item) {
+    return res.json(item);
+  }
+  res.status(404).json({error: 'Not found'});
+});
+
+router.post('/', (req, res) => {
+  const note = create( title = '', text = '', datetime = '');
+
+  if(note.title.length === 0){
+    return res.status(400).json({
+      field: 'title',
+      error: 'Title must be a non-empty string',
+    });
+  }
+  if(text.length === 0){
+    return res.status(400).json({
+      field: 'title',
+      error: 'Title must be a non-empty string',
+    });
+  }
+  if(datetime.length === 0){
+    return res.status(400).json({
+      field: 'title',
+      error: 'Title must be a non-empty string',
+    });
+  }
+
+  const nextId = data.map(i => i.id).reduce((a, b) => a > b ? a : b + 1, 1);
+
+  const item = { id: nextid, title: title, datetime: datetime};
+  data.push(item);
+
+  return res.status(201).json(item);
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  create( { title: '', text: '', datetime: ''}) = req.body;
+
+  if(title.length === 0){
+    return res.status(400).json({
+      field: 'title',
+      error: 'Title must be a non-empty string',
+    });
+  }
+  const item = data.find(i => i.id === parseInt(id, 10));
+
+  if(item) {
+    item.title = title;
+    return res.status(200).json(item);
+  }
+
+  res.status(404).json({error: 'Not found'});
+
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  
+  const item = data.find(i => i.id === parseInt(id, 10));
+
+  if(item){
+    data.splice(data.indexOf(item), 1);
+    return res.status(204).end();
+  }
+  res.status(404).json({error: 'Not found'});
+});
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,6 +1,26 @@
 /* todo sækja pakka sem vantar  */
 
+const bcrypt = require('bcrypt');
+const { Client } = require('pg');
 const connectionString = process.env.DATABASE_URL;
+
+
+async function query(q, values = []) {
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  let result;
+
+  try {
+    result = await client.query(q, values);
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+
+  return result;
+}
 
 /**
  * Create a note asynchronously.
@@ -13,7 +33,10 @@ const connectionString = process.env.DATABASE_URL;
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function create({ title, text, datetime } = {}) {
-  /* todo útfæra */
+  const q = 'INSERT INTO notes (title, text, datetime) VALUES ($1, $2, $3) RETURNING *'
+  const result = await query(q, [title, text, datetime]);
+  console.log(result);
+  return result.rows[0];
 }
 
 /**
@@ -22,7 +45,6 @@ async function create({ title, text, datetime } = {}) {
  * @returns {Promise} Promise representing an array of all note objects
  */
 async function readAll() {
-  'SELECT * FROM notes'
 }
 
 /**
